f1014cedee6acde3579c03faf9a84dee4c1ca41c
AdamT-TLee <adam832lee@gmail.com>
2026-07-01 01:13:39 -0500

    setup lsp config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "omnisharp", "clangd" },
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.enable({ "lua_ls", "omnisharp", "clangd" })
+    end,
+  },
+}
