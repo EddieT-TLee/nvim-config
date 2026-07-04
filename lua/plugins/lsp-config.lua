4931cf64331578bfc20c0e6cbb5d8d54624d0412
AdamT-TLee <adam832lee@gmail.com>
2026-07-03 23:05:05 -0500

    Added Snippets and autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,7 +17,28 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "hrsh7th/cmp-nvim-lsp" },
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+			})
+
+			vim.lsp.config("omnisharp", {
+				cmd = { "omnisharp" },
+				capabilities = capabilities,
+				settings = {
+					omnisharp = {
+						useModernNet = false,
+					},
+				},
+			})
+
+			vim.lsp.config("clangd", {
+				capabilities = capabilities,
+			})
+
 			vim.lsp.enable({ "lua_ls", "omnisharp", "clangd" })
 		end,
 	},
